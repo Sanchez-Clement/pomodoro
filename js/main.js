06d9cf61bd9b292a232a968d6cc98e759e57ea25
Clement Sanchez <sanchez.clmnt@gmail.com>
2017-07-20 10:46:36 +0200

    main.js : make the function session and compteur session

--- js/main.js
+++ js/main.js
@@ -1,15 +1,82 @@
 var minuteSession= 0;
 var minuteBreak = 30;
-var secondeSession = 0;
-var secondeBreak = 0;
-
+var secondeSession = -1;
+var secondeBreak = 59;
+// ***********************affichage de départ****************************************
 
 $("#break .time").text(minuteBreak);
+$("#compteurBreak .compteur p").text(minuteBreak);
 $("#session .time").text(minuteSession);
-$("#break p:first").click(function() {minuteBreak--;$("#break .time").text(minuteBreak);})
-$("#break p:last").click(function() {minuteBreak++;$("#break .time").text(minuteBreak);})
-$("#session p:first").click(function() {minuteSession--;$("#session .time").text(minuteSession);})
-$("#session p:last").click(function() {minuteSession++;$("#session .time").text(minuteSession);})
+$("#compteurSession .compteur p").text(minuteSession);
 
-// $("#session p:first").click(function() {minuteSession--;$("#session .time").text(minuteSession)};)
-// $("#session p:last").click(function() {minuteSession++;$("#session .time").text(minuteSession);})
+// *************************************augmenter ou diminuer compteur break***************************
+$("#break p:first").click(function() {
+  minuteBreak--;
+  $("#break .time").text(minuteBreak);
+$("#compteurBreak .compteur p").text(minuteBreak);
+})
+
+
+$("#break p:last").click(function() {
+  minuteBreak++;
+  $("#break .time").text(minuteBreak);
+$("#compteurBreak .compteur p").text(minuteBreak);
+})
+// *************************************augmenter ou diminuer compteur session***************************
+
+
+$("#session p:first").click(function() {
+  minuteSession--;
+  $("#session .time").text(minuteSession);
+$("#compteurSession .compteur p").text(minuteSession);
+})
+
+$("#session p:last").click(function() {
+  minuteSession++;
+  $("#session .time").text(minuteSession);
+$("#compteurSession .compteur p").text(minuteSession);
+})
+
+
+
+
+// **************************************début des fonction***********************************
+
+
+/**
+ * compteurSession - chronometre pour session;
+ *
+ * @return {type}  no return
+ */
+function compteurSession() {
+
+
+  secondeSession --;
+  if (secondeSession < 0) {
+    secondeSession = 59;
+    minuteSession --;
+
+  }
+
+$("#compteurSession .compteur p").text(minuteSession + ":" + secondeSession);
+}
+
+
+
+
+/**
+ * session - lance le chronometre pour la session
+ *
+ * @return {type}  no return
+ */
+function session() {
+// $("#compteurSession .compteur p").text(minuteSession + ":" + secondeSession);
+setInterval(compteurSession, 1000);
+
+}
+
+
+// **********************fin des fonctions**************************************
+
+
+$("#compteurSession").click(session);
