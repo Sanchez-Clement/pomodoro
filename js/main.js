f6b1287434d723e733c239bd05257dc67d0bb4c5
Clement Sanchez <sanchez.clmnt@gmail.com>
2017-07-20 15:30:34 +0200

    add the slidedown for green and rad

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 var minuteSession= 0;
 var minuteBreak = 30;
 var secondeSession = -1;
-var secondeBreak = 59;
+var secondeBreak = -1;
+var clearBreak;
+var clearSession;
 // ***********************affichage de départ****************************************
 
 $("#break .time").text(minuteBreak);
@@ -50,6 +52,7 @@ $("#compteurSession .compteur p").text(minuteSession);
  */
 function compteurSession() {
 
+// alert(minuteBreak);
 
   secondeSession --;
   if (secondeSession < 0) {
@@ -59,6 +62,36 @@ function compteurSession() {
   }
 
 $("#compteurSession .compteur p").text(minuteSession + ":" + secondeSession);
+if (secondeSession ==0 && minuteSession ==0) {
+  secondeSession = -1;
+  minuteSession = $("#session .time").text() - 1;
+  clearInterval(clearSession);
+
+  lanceBreak();
+
+}
+}
+
+function compteurBreak() {
+// minuteBreak = $("#break .time").val;
+// alert("non");
+  secondeBreak --;
+  if (secondeBreak < 0) {
+    secondeBreak = 59;
+    minuteBreak --;
+
+  }
+
+$("#compteurBreak .compteur p").text(minuteBreak + ":" + secondeBreak);
+if (secondeBreak ==0 && minuteBreak ==0) {
+  secondeBreak = -1;
+  // minuteBreak = $("#break .time").text() - 1;
+  // alert("oui");
+  clearInterval(clearBreak);
+
+session();
+
+}
 }
 
 
@@ -70,9 +103,27 @@ $("#compteurSession .compteur p").text(minuteSession + ":" + secondeSession);
  * @return {type}  no return
  */
 function session() {
+  minuteSession = $("#session .time").text();
+  alert(minuteSession);
 // $("#compteurSession .compteur p").text(minuteSession + ":" + secondeSession);
-setInterval(compteurSession, 1000);
+clearSession =setInterval(compteurSession, 1000);
+$("#compteurBreak").hide();
+$("#red").slideUp(1000);
+$("#compteurSession").show();
+ $("#green").slideDown(minuteSession * 60000);
+}
 
+function lanceBreak() {
+
+minuteBreak = $("#break .time").text();
+alert(minuteBreak);
+// $("#compteurSession .compteur p").text(minuteSession + ":" + secondeSession);
+clearBreak = setInterval(compteurBreak, 1000);
+$("#compteurSession").hide();
+$("#green").slideUp(1000);
+$("#compteurBreak").show();
+
+$("#red").slideDown(minuteBreak * 60000);
 }
 
 
