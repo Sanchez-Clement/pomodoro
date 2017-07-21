7308d6b5519bca4ce8532b1f2be6bf15e7522a5f
Clement Sanchez <sanchez.clmnt@gmail.com>
2017-07-21 14:04:39 +0200

    main.js m make the function stopBrean

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var minuteSession= 1;
+var minuteSession = 1;
 var minuteBreak = 1;
 var secondeSession = -1;
 var secondeBreak = -1;
@@ -16,43 +16,47 @@ $("#compteurSession .compteur p").text(minuteSession);
 
 // *************************************augmenter ou diminuer compteur break***************************
 function selectClick() {
-$("#break p:first").click(function() {
-  if(minuteBreak >1) {
-  minuteBreak--;}
-  secondeBreak = -1;
-  $("#break .time").text(minuteBreak);
-$("#compteurBreak .compteur p").text(minuteBreak);
-})
+  $("#break p:first").click(function() {
+    if (minuteBreak > 1) {
+      minuteBreak--;
+    }
+    secondeBreak = -1;
+    $("#break .time").text(minuteBreak);
+    $("#compteurBreak .compteur p").text(minuteBreak);
+    $("#red").slideUp(1000);
+  })
 
 
-$("#break p:last").click(function() {
-  minuteBreak++;
-  secondeBreak = -1;
-  $("#break .time").text(minuteBreak);
-$("#compteurBreak .compteur p").text(minuteBreak);
-})
+  $("#break p:last").click(function() {
+    minuteBreak++;
+    secondeBreak = -1;
+    $("#break .time").text(minuteBreak);
+    $("#compteurBreak .compteur p").text(minuteBreak);
+    $("#red").slideUp(1000);
+  })
 
 
-// *************************************augmenter ou diminuer compteur session***************************
+  // *************************************augmenter ou diminuer compteur session***************************
 
 
-$("#session p:first").click(function() {
-  if(minuteSession > 1) {
-  minuteSession--;}
-  secondeSession = -1;
-  $("#session .time").text(minuteSession);
-$("#compteurSession .compteur p").text(minuteSession);
-$("#green").slideUp(1000);
-})
+  $("#session p:first").click(function() {
+    if (minuteSession > 1) {
+      minuteSession--;
+    }
+    secondeSession = -1;
+    $("#session .time").text(minuteSession);
+    $("#compteurSession .compteur p").text(minuteSession);
+    $("#green").slideUp(1000);
+  })
 
-$("#session p:last").click(function() {
+  $("#session p:last").click(function() {
 
-  minuteSession++;
-  secondeSession = -1;
-  $("#session .time").text(minuteSession);
-$("#compteurSession .compteur p").text(minuteSession);
-$("#green").slideUp(1000);
-})
+    minuteSession++;
+    secondeSession = -1;
+    $("#session .time").text(minuteSession);
+    $("#compteurSession .compteur p").text(minuteSession);
+    $("#green").slideUp(1000);
+  })
 
 }
 
@@ -69,68 +73,65 @@ function compteurSession() {
 
 
 
-// // alert("oui");
+  $("#selecteurs p").off("click");
 
 
-// alert(minuteBreak);
-$("#selecteurs p").off("click");
-
-
-  secondeSession --;
+  secondeSession--;
   if (secondeSession < 0) {
     secondeSession = 59;
-    minuteSession --;
+    minuteSession--;
 
   }
 
-$("#compteurSession .compteur p").text(minuteSession + ":" + secondeSession);
+  $("#compteurSession .compteur p").text(minuteSession + ":" + secondeSession);
 
-if (secondeSession < 10) {
-  $("#compteurSession .compteur p").text(minuteSession + ": 0" + secondeSession);
-}
+  if (secondeSession < 10) {
+    $("#compteurSession .compteur p").text(minuteSession + ": 0" + secondeSession);
+  }
 
 
 
-if (secondeSession ==0 && minuteSession ==0) {
-  secondeSession = -1;
-  minuteSession = $("#session .time").text() - 1;
-  clearInterval(clearSession);
+  if (secondeSession == 0 && minuteSession == 0) {
+    secondeSession = -1;
+    minuteSession = $("#session .time").text() - 1;
+    clearInterval(clearSession);
+    clickCompteur = 0;
 
-  lanceBreak();
+    lanceBreak();
 
-}
+  }
 }
 
 function compteurBreak() {
-// minuteBreak = $("#break .time").val;
-// alert("non");
+  // minuteBreak = $("#break .time").val;
+  // alert("non");
+  $("#selecteurs p").off("click");
 
 
 
-
-  secondeBreak --;
+  secondeBreak--;
   if (secondeBreak < 0) {
     secondeBreak = 59;
-    minuteBreak --;
+    minuteBreak--;
 
   }
 
-$("#compteurBreak .compteur p").text(minuteBreak + ":" + secondeBreak);
+  $("#compteurBreak .compteur p").text(minuteBreak + ":" + secondeBreak);
 
-if (secondeBreak < 10) {
-  $("#compteurBreak.compteur p").text(minuteBreak + ": 0" + secondeBreak);
-}
+  if (secondeBreak < 10) {
+    $("#compteurBreak .compteur p").text(minuteBreak + ": 0" + secondeBreak);
+  }
 
-if (secondeBreak ==0 && minuteBreak ==0) {
-  secondeBreak = -1;
-  // minuteBreak = $("#break .time").text() - 1;
-  // alert("oui");
-  clearInterval(clearBreak);
-  compteurSession=0;
+  if (secondeBreak == 0 && minuteBreak == 0) {
+    secondeBreak = -1;
+    // minuteBreak = $("#break .time").text() - 1;
+    // alert("oui");
+    clearInterval(clearBreak);
+    clickCompteur = 0;
 
-session();
+    session();
 
-}
+  }
 }
 
 
@@ -142,54 +143,79 @@ session();
  * @return {type}  no return
  */
 function session() {
-clickCompteur++;
+  clickCompteur++;
 
 
-if (clickCompteur ==1) {
+  if (clickCompteur == 1) {
 
 
-  minuteSession = $("#session .time").text();
+    minuteSession = $("#session .time").text();
 
-clearSession =setInterval(compteurSession, 1000);
-$("#compteurBreak").hide();
-$("#red").slideUp(1000);
-$("#compteurSession").show();
- $("#green").slideDown(minuteSession * 60000);
+    clearSession = setInterval(compteurSession, 1000);
+    $("#compteurBreak").hide();
+    $("#red").slideUp(1000);
+    $("#compteurSession").show();
+    $("#green").slideDown(minuteSession * 60000);
 
-}
+  }
 
-if (clickCompteur %2 == 0) {
+  if (clickCompteur % 2 == 0) {
 
-  $("#compteurSession").click(pauseSession());
-} else if( clickCompteur %2 != 0  && clickCompteur !=1 ){
-  repriseSession();
-}
-console.log(clickCompteur);
+    $("#compteurSession").click(pauseSession());
+  } else if (clickCompteur % 2 != 0 && clickCompteur != 1) {
+    repriseSession();
+  }
+
 
 
 }
 
 function lanceBreak() {
+  // alert("oui")
+  clickCompteur++;
+  // console.log(clickCompteur);
 
-minuteBreak = $("#break .time").text();
-// alert(minuteBreak);
-// $("#compteurSession .compteur p").text(minuteSession + ":" + secondeSession);
-clearBreak = setInterval(compteurBreak, 1000);
-$("#compteurSession").hide();
-$("#green").slideUp(1000);
-$("#compteurBreak").show();
 
-$("#red").slideDown(minuteBreak * 60000);
+  if (clickCompteur == 1) {
+    minuteBreak = $("#break .time").text();
+    clearBreak = setInterval(compteurBreak, 1000);
+    $("#compteurSession").hide();
+    $("#green").slideUp(1000);
+    $("#compteurBreak").show();
+
+    $("#red").slideDown(minuteBreak * 60000);
+  }
+
+  if (clickCompteur % 2 == 0) {
+
+    $("#compteurBreak").click(pauseBreak());
+  } else if (clickCompteur % 2 != 0 && clickCompteur != 1) {
+    repriseBreak();
+  }
+
+
+  // alert(minuteBreak);
+  // $("#compteurSession .compteur p").text(minuteSession + ":" + secondeSession);
+
+
 }
 
 
 function pauseSession() {
-clickselecteur=0;
-testminute = minuteSession;
-console.log(testminute);
 
-   $("#green").stop();
+
+  $("#green").stop();
   clearInterval(clearSession);
+  selectClick();
+
+
+}
+
+function pauseBreak() {
+
+
+  $("#red").stop();
+  clearInterval(clearBreak);
   selectClick();
 
 
@@ -198,14 +224,27 @@ console.log(testminute);
 function repriseSession() {
 
   $("#green").slideDown(minuteSession * 60000 + secondeSession * 1000);
-console.log(minuteSession,secondeSession);
-  clearSession =setInterval(compteurSession, 1000);
+
+  clearSession = setInterval(compteurSession, 1000);
+}
+
+function repriseBreak() {
+
+  $("#red").slideDown(minuteBreak * 60000 + secondeBreak * 1000);
+
+  clearBreak = setInterval(compteurBreak, 1000);
 }
 
 
 // **********************fin des fonctions**************************************
 selectClick();
 
-if (clickCompteur==0) {
+if (clickCompteur == 0) {
 
-$("#compteurSession").click(session);}
+  $("#compteurSession").click(session);
+}
+
+if (clickCompteur == 0) {
+
+  $("#compteurBreak").click(lanceBreak);
+}
